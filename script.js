b770412813bf3d5b82f9f0e4c18596ab47d95184
josebonilla9 <jose1999001@hotmail.com>
2024-12-16 11:19:35 +0100

    Actualizando funcionalidades

--- script.js
+++ script.js
@@ -141,28 +141,39 @@ function closeAddTaskModal() {
     document.getElementById('addTaskModal').style.display = 'none';
 }
 
-// Function to delete a task
-function deleteTask(taskElement) {
-    // Confirmation dialog to confirm deletion
-    if (confirm("Are you sure you want to delete this task?")) {
-        // If user confirms, remove the task element from its parent
-        taskElement.parentNode.removeChild(taskElement);
-    }
-}
-
-// Function to edit a task
-function editTask(taskElement) {
-    // Prompt user to edit the task description, with current description as default
-    const newTaskDesc = prompt("Edit your task:", taskElement.textContent);
-    // Check if user entered a new task description and it's not empty
-    if (newTaskDesc !== null & newTaskDesc.trim() !== "") {
-        // Update task element's text content with the new description
-        taskElement.textContent = newTaskDesc;
-    }
-}
-
 // Function to add a task
-function addTask() {
+function addTrade() {
+    // Get task date and description from input fields
+    const taskDate = new Date(document.getElementById('task-date').value);
+    const taskDesc = document.getElementById('task-desc').value.trim();
+
+    // Validate task date and description
+    if (taskDesc && !isNaN(taskDate.getDate())) {
+        // Get calendar days
+        const calendarDays = document.getElementById('calendar').children;
+        // Iterate through calendar days
+        for (let i = 0; i < calendarDays.length; i++) {
+            const day = calendarDays[i];
+            // Check if day matches task date
+            if (parseInt(day.textContent) === taskDate.getDate()) {
+                // Create task element
+                const taskElement = document.createElement("div");
+                taskElement.className = "task";
+                taskElement.textContent = taskDesc;
+
+                // Append task element to day element
+                day.appendChild(taskElement);
+                break;
+            }
+        }
+        closeAddTaskModal(); // Close add task modal
+    } else {
+        // Alert if invalid date or task description
+        alert("Please enter a valid date and task description!");
+    }
+}
+
+function editDay() {
     // Get task date and description from input fields
     const taskDate = new Date(document.getElementById('task-date').value);
     const taskDesc = document.getElementById('task-desc').value.trim();
